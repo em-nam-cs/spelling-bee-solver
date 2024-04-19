b5ed8dce46b2f16b45f944e9739aad730e8b324a
Em Nam <78702559+Em-Nam@users.noreply.github.com>
2024-04-18 21:13:52 -0700

    Add function headers

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 /**
-@brief
+@brief program that solves the New York Times Spelling Bee game.
+    Given a list of letters and a target letter, find all the valid words
+    that can be created that include the target letter. Letters can be reused
+    as frequently as needed
+
+Note: Words and letters are compared in lowercase (have this built in so no user issues)
 
 @todo use a larger legit ditionary
 @todo optimize search dictionary in sections and cut out words that don't start with one
 of the letters
-@todo filter out words that are too short
+@todo filter out words that are too short (based on a variable)
+@todo indicate the panagrams that use all letters
 @todo get rid of print statements
 @todo create a ui
-@todo indicate the panagrams that use all letters
+@todo create automated testing, more testing ex
 
-Words and letters are compared in lowercase
+
+
 
 @references NYT Spelling Bee Game
 @author Em Nam
@@ -18,8 +25,8 @@ Words and letters are compared in lowercase
 
 const filesystem = require("fs");
 
-const letters = ["c", "a", "t"];
-const target = "a";
+const letters = ["t", "o", "l", "k", "b", "i", "n"];
+const target = "b";
 const miniDict = [
     "act",
     "cat",
@@ -33,24 +40,35 @@ const miniDict = [
 ];
 const dictionary = readDictionary("assets/dictionary.txt");
 
-// for (let i = 0; i < 20; i++) {
-//     console.log(dictionary[i]);
-// }
-
 const wordsTarget = generateAllWordsWithTarget(letters, miniDict, target);
 const wordsTargetBig = generateAllWordsWithTarget(letters, dictionary, target);
 console.log("Words with target");
 console.log(wordsTarget);
 console.log(wordsTargetBig);
 
-//read list of words from a dictionary txt file and return the array of words
-//assume each word is deliniated by a newline
+/**
+ * reads a list of words from a text file and returns array
+ *      assume that each word is deliniated by a newline
+ * @param {*} file name of the path/file that is being read in
+ * @returns an array that stores all of the words in the file
+ */
 function readDictionary(file) {
     return filesystem.readFileSync(file).toString().toLowerCase().split("\n");
 }
 
+/**
+ * for each word in the dictionary, determine if it is a valid word to create
+ *      and generate and return an array of these words. Valid created words
+ *      must be made of only the given letters. Letters can be reused as many times
+ *      as per NYT Spelling Bee rules. The valid created words must also contain
+ *      the target letter at least once
+ * @param {*} letters array of available letters
+ * @param {*} dict array of possible valid words
+ * @param {*} target the letter that must appear in the word
+ * @returns
+ */
 function generateAllWordsWithTarget(letters, dict, target) {
-    console.log(`${letters}, ${dict}, ${target}`);
+    // console.log(`${letters}, ${dict}, ${target}`);
     const words = [];
     for (let i = 0; i < dict.length; i++) {
         // console.log(`checking word ${dict[i]}`);
@@ -62,7 +80,19 @@ function generateAllWordsWithTarget(letters, dict, target) {
     return words;
 }
 
-// assume valid initially false
+/**
+ * Determine if the word is a valid word to create using the letters and
+ *      given target.Valid created words must be made of only the given letters.
+ *      Letters can be reused as many times as per NYT Spelling Bee rules.
+ *      The valid created words must also contain
+ *      the target letter at least once
+ * @param {*} letters array of available letters
+ * @param {*} word string of a word that is being checked if it can be created
+ * @param {*} target the letter that must appear in the word
+ * @param {*} valid boolean that is initially false until the target letter is
+ *      used in the word
+ * @returns
+ */
 function checkWordWithTarget(letters, word, target, valid) {
     if (word == "") {
         return valid;
